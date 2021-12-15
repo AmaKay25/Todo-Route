ef8b5c1a3eb9e5c19e62f213733b5e6f68ac2762
AmaKay25 <takyi.sandra23@gmail.com>
2021-12-15 11:58:15 +0000

    testing api

--- index.js
+++ index.js
@@ -11,13 +11,18 @@ const db = process.env.DB_URL;
 
 mongoose.connect(db,
     {useNewUrlParser:true,useUnifiedTopology:true})
-    .then(() => {console.log('database is running live')})
+    .then(() => {console.log('database is running live')}
+    )
 const port = 3000;
 
+
+app.use(cors());
 app.use(express.json())
-app.use('/todos', router)
+app.use('/todos', router);
 
-
+app.get('/',(req,res) => {
+    res.send('welcome to She Writes tutorial');
+})
 app.listen(port, () => {
     console.log (`listening at port ${port}`);
 })
